ea38c69bed7fac5da86dde691813d8c2847b13cd
helder rocha <hedie.strokes@gmail.com>
2016-04-04 20:38:31 -0300

    Adicionado chaves no obj Tokens

--- tokens.php
+++ tokens.php
@@ -8,17 +8,76 @@ class Tokens
 	function getTokens() {
 
 		$token = new Token();
-		$token->name = 'a';
+		$token->name = 'PROGRAM';
 		$token->codigo = '1';
 
 		$arrayTokens[] = $token;
 
 		$token = new Token();
-		$token->name = 'b';
+		$token->name = 'LABEL';
 		$token->codigo = '2';
 
 		$arrayTokens[] = $token;
 
+		$token = new Token();
+		$token->name = 'CONST';
+		$token->codigo = '3';
+
+		$arrayTokens[] = $token;
+
+		$token = new Token();
+		$token->name = 'VAR';
+		$token->codigo = '4';
+
+		$arrayTokens[] = $token;
+
+				$token = new Token();
+		$token->name = 'PROCEDURE';
+		$token->codigo = '5';
+
+		$arrayTokens[] = $token;
+
+		$token = new Token();
+		$token->name = 'BEGIN';
+		$token->codigo = '6';
+
+		$arrayTokens[] = $token;
+
+				$token = new Token();
+		$token->name = 'END';
+		$token->codigo = '7';
+
+		$arrayTokens[] = $token;
+
+		$token = new Token();
+		$token->name = 'INT';
+		$token->codigo = '8';
+
+		$arrayTokens[] = $token;
+
+				$token = new Token();
+		$token->name = 'ARRAY';
+		$token->codigo = '9';
+
+		$arrayTokens[] = $token;
+
+		$token = new Token();
+		$token->name = 'OF';
+		$token->codigo = '10';
+
+		$arrayTokens[] = $token;
+
+				$token = new Token();
+		$token->name = 'CALL';
+		$token->codigo = '11';
+
+		$arrayTokens[] = $token;
+
+		$token = new Token();
+		$token->name = 'GOTO';
+		$token->codigo = '12';
+
+		$arrayTokens[] = $token;
 		return $arrayTokens;
 	}
 }
