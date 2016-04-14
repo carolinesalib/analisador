f64f1ada69ad462218efbd5fa9de76c1ed724238
Caroline Salib Canto <carolinesalibc@gmail.com>
2016-04-14 13:18:18 -0300

    Corrigido para verificar corretamente e eliminar os comentários;

--- comentarios.php
+++ comentarios.php
@@ -7,31 +7,49 @@ class Comentarios
 
 	static function Remove($codigoFonte)
 	{
-		//Varre todos os caracteres procurando por tag de comentário
-		for ($i=0; $i < strlen($codigoFonte); $i++) {
+		$codigoFonte = Comentarios::RemoveLinha($codigoFonte);
+		$codigoFonte = Comentarios::RemoveBloco($codigoFonte);
 
-			//Se for comentário de linha, elimina a linha
+		return $codigoFonte;
+	}
+
+	static function RemoveLinha($codigoFonte) {
+
+		for ($i=0; $i < strlen($codigoFonte); $i++) {
+			if (($i+2)> strlen($codigoFonte)) break;
+
 			if (substr($codigoFonte, $i, 2) == "//") {
 				$posicaoTag = strpos($codigoFonte, "//");
 				$stringDepoisTag = substr($codigoFonte, $posicaoTag);
      			$posicaoQuebraLinha = stripos($stringDepoisTag, "\n");
+
+     			if($posicaoQuebraLinha == 0) $posicaoQuebraLinha = strlen($codigoFonte);
+
      			$stringComentarioLinha = substr($stringDepoisTag, 0, $posicaoQuebraLinha);
 
 				$codigoFonte = str_replace($stringComentarioLinha, "", $codigoFonte);
 
-				Comentarios::Remove($codigoFonte);
-			} else
-			//Se for comentário de bloco, elimina o bloco
+				return Comentarios::RemoveLinha($codigoFonte);
+			}
+		}
+		return $codigoFonte;
+	}
+
+	function RemoveBloco($codigoFonte) {
+
+		for ($i=0; $i < strlen($codigoFonte); $i++) {
 			if (substr($codigoFonte, $i, 2) == "/*") {
 				$posicaoTag = strpos($codigoFonte, "/*");
-				$posicaoFinalTag = strpos($codigoFonte, "*/")+2;
-     			$stringComentarioLinha = substr($codigoFonte, $posicaoTag, $posicaoFinalTag);
+				$stringDepoisTag = substr($codigoFonte, $posicaoTag);
+				$posicaoFinalTag = strpos($stringDepoisTag, "*/")+2;
+     			$stringComentarioLinha = substr($stringDepoisTag, 0, $posicaoFinalTag);
 
 				$codigoFonte = str_replace($stringComentarioLinha, "", $codigoFonte);
 
-				Comentarios::Remove($codigoFonte);
+				return Comentarios::RemoveBloco($codigoFonte);
 			}
 		}
+
 		return $codigoFonte;
 	}
 }
